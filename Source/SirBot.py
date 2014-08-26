23f2b1c716f0c3bfbdbd8f91c973401994abcdb9
SirRujak <sirrujak@gmail.com>
2014-08-26 18:16:54 -0500

    Starter stuff for the new command structure.
    This should have gone up long ago :P

--- Source/SirBot.py
+++ Source/SirBot.py
@@ -23,6 +23,8 @@ defaultState = 1
 
 
 ## These must currently start with ! for commands.
+## This part will do nothing in the very near future! Soon to be removed
+## in favor of the new command system being implemented.
 def checkChatCommand(channelName, chatData):
         if( chatData=="!info" ):
                 message = "Watch both of us play WildStar here!: "
@@ -57,10 +59,34 @@ def checkChatWelcome(channelName, userName):
 ###############################################################
 ###############################################################
 
+## NEW COMMAND PARSING AREA
+class chatHandler:
+        ## Needs variables for chatData, command dictionary
+        ## Needs functions for loading & unloading command dictionary
+        ## Needs fucntions for checking the kind of command it is
+        ## Needs functions for splitting up multi commands
+        ## Needs functions for adding and removing things from queues
+        ## Needs variables for in and out queues
+        ## Needs to implement parser and chat level checker
+        ## Needs to be able to run at arbitrary chat level
+        ## Needs to be able to make a timer
+        ## Needs to have all of the base functions made
+        ## Base functions are timeout, ban, promote to level, demote to level,
+        ## Needs channelName, temp, modList, spamLevel, spamFilter variables where temp is the message
+        def __init__():
+                pass
+        
+        def checkChatCMD(chatData):
+                pass
+
+        def splitMultiCMD(multiCMD):
+                pass
 
 
 
 
+###############################################################
+###############################################################
 
 
 
