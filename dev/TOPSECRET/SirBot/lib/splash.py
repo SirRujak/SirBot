4e6d9eebffddf2e7115096603c681cc6120eee17
avoloc <avoloc.dev@hotmail.com>
2014-09-09 15:23:11 -0500

    initial commit of splash.py
    script for loading splash screen on load up

--- dev/TOPSECRET/SirBot/lib/splash.py
+++ dev/TOPSECRET/SirBot/lib/splash.py
@@ -1,1 +1,51 @@
 #simple script for importing splash assets and displaying splash screen
+
+from tkinter import ttk
+from tkinter import PhotoImage
+from tkinter import Tk
+from tkinter import Label
+
+def splash():
+    try:
+        splashimage = PhotoImage(file='splash.gif')
+        loadingSplash = Tk()
+        loading = ttk.Label(loadingSplash,image=self.splashimage)
+        loadingSplash.overrideredirect(True)
+        loading.pack()
+
+        top = loading.winfo_toplevel()
+        h = loading.winfo_screenheight()
+        w = loading.winfo_screenwidth()
+
+        loadingSplash.wm_attributes('-alpha',0.75)
+        loadingSplash.update_idletasks()
+        loadingSplash.geometry('262x112+'+str(int(w/2)-131*1)+
+                                    '+'+str(int(h/2)-56*1))
+        loadingSplash.update_idletasks()
+        loadingSplash.update()
+    except:
+        #log
+        loadingSplash = Tk()
+        myfont = tkFont.families()[0]
+        loading = Label(loadingSplash,text='SirBot')
+        loadingSplash.overrideredirect(True)
+        loading.pack()
+
+        top = lading.winfo_toplevel()
+        h = loading.winfo_screenheight()
+        w = loading.winfo_screenwidth()
+
+        loadingSplash.wm_attributes('-alpha',0.75)
+        loadingSplash.update_idletasks()
+        loadingSplash.geometry('262x112+'+str(int(w/2)-131*1)+
+                               '+'+str(int(h/2)-56*1))
+        loadingSplash.update_idletasks()
+        loadingSplash.update()
+
+def destroy():
+    try:
+        loadingSplash.destroy()
+    except:
+        #log
+        pass
+    
