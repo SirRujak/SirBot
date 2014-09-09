a86ca226d78ef614e0b11fda45e7dcf0bf2e7322
avoloc <avoloc.dev@hotmail.com>
2014-09-09 16:37:24 -0500

    directory shuffle
    slightly reshuffled file hierarchy to be more consistent

---
+++ dev/TOPSECRET/SirBot/lib/sirbot/dataloader.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#simple script for loading configurations into memory
+
+from json import loads
+from os.path import dirname
+
+
+
+def dataloader():
+    moduleDir=dirname(__file__)
+    moduleDir=moduleDir.rsplit('/',2)[0]
+    configPath=moduleDir+'/config/sirbot/config'
+    
+    configFile = open(configPath,"rb+")
+    data = configFile.read().decode()
+    data = loads(data)
+    configFile.close()
+
+    return(data)
---
+++ dev/TOPSECRET/SirBot/lib/sirbot/validator.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+#script that validates sirbot installation
