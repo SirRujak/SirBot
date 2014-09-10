ad8dab46cbbf736e348c3f691f0f3f703bfeb886
avoloc <avoloc@hotmail.com>
2014-09-10 17:24:03 -0500

    added a note to validator.py
    reminder for elements of validator.py

--- dev/TOPSECRET/SirBot/lib/sirbot/validator.py
+++ dev/TOPSECRET/SirBot/lib/sirbot/validator.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 
 #script that validates sirbot installation
+
+#check for clean.start file in config directory
+#if exists: skip careful checking; do just minimal import checking
