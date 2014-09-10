256b28ce1b556772cd6f1ddb7116cb78ada59f80
avoloc <avoloc.dev@hotmail.com>
2014-09-10 16:11:07 -0500

    initial commit of assetloader.py
    assetloader.py is a script designed to preload audio/visual assets for
    sirbot runtime

--- dev/TOPSECRET/SirBot/lib/sirbot/assetloader.py
+++ dev/TOPSECRET/SirBot/lib/sirbot/assetloader.py
@@ -1,1 +1,73 @@
+# -*- coding: utf-8 -*-
+
 #script for loading all audio and visual sirbot assets - except for splash assets
+
+from tkinter import PhotoImage
+
+def assetloader():
+    moduleDir=dirname(__file__)
+    moduleDir=moduleDir.rsplit('/',2)[0]
+    assetPath=moduleDir+'/resource/sirbot/'
+    
+    configFile = open(configPath,"rb+")
+    data = configFile.read().decode()
+    data = loads(data)
+    configFile.close()
+
+    try:
+        filename = assetPath+'Sirbot.gif'
+        self.logoimage = tk.PhotoImage(file=filename)
+    except:
+        #log
+        pass
+    try:
+        filename = assetPath+'userslist.gif'
+        self.userslistimage = tk.PhotoImage(file=filename)
+    except:
+        #log
+        pass
+    try:
+        filename = assetPath+'options.gif'
+        self.optionsimage = tk.PhotoImage(file=filename)
+    except:
+        #log
+        pass
+    try:
+        filename = assetPath+'dashboard.gif'
+        self.dashboardimage = tk.PhotoImage(file=filename)
+    except:
+        #log
+        pass
+    try:
+        filename = assetPath+'chat.gif'
+        self.chatimage = tk.PhotoImage(file=filename)
+    except:
+        #log
+        pass
+    try:
+        filename = assetPath+'users.gif'
+        self.usersimage = tk.PhotoImage(file=filename)
+    except:
+        #log
+        pass
+    try:
+        filename = assetPath+'commands.gif'
+        self.commandsimage = tk.PhotoImage(file=filename)
+    except:
+        #log
+        pass
+    try:
+        filename = assetPath+'advanced.gif'
+        self.advancedimage = tk.PhotoImage(file=filename)
+    except:
+        #log
+        pass
+    try:
+        filename = assetPath+'help.gif'
+        self.helpimage = tk.PhotoImage(file=filename)
+    except:
+        #log
+        pass
+
+
+    return(data)
