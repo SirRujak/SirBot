fcaa5c9b8e3d43706d9bb48592cb1b53bfe1e24f
avoloc <avoloc@hotmail.com>
2014-09-10 18:00:06 -0500

    initial commit of ai.py
    timer class

--- dev/TOPSECRET/SirBot/lib/sirbot/ai.py
+++ dev/TOPSECRET/SirBot/lib/sirbot/ai.py
@@ -1,2 +1,22 @@
+# -*- coding: utf-8 -*-
+
 #classes containing machinery for artifically intelligent operations
 #i.e. data parsing and command execution
+
+class baseTimer():
+        def __init__(self, prevTime, currTime, timerLen):
+                self.prevTime = prevTime
+                self.currTime = currTime
+                self.timerLen = timerLen
+                self.timePassed = 0
+        def setCurrTime(self, currTime):
+                self.currTime = currTime
+        def setTimerLen(self, timerLen):
+                self.timerLen = timerLen
+        def checkIfTimePassed(self):
+                timePass = self.currTime - self.prevTime
+                if ( timePass >= self.timerLen):
+                        self.timePassed = 1
+                else:
+                        self.timePassed = 0
+
