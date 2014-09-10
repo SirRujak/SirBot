630394f53da540541418b801641e515d79de5d46
avoloc <avoloc@hotmail.com>
2014-09-10 17:20:47 -0500

    further implemented main.py module
    added imports

--- dev/TOPSECRET/SirBot/lib/sirbot/main.py
+++ dev/TOPSECRET/SirBot/lib/sirbot/main.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
 #class that contains calls to mainloop/updatetasks of subordinate classes
 #implement prioritization and optimization structures
 #may contain elements to monitor system utilization
+
+#import all subordinate modules
+def imports():
+    import data
+    import interface
+    import network
+    import ai
+    import twitch
+    import irc
+    import logger
+    import player
+    import streamer
+    import obs
+    import voice
+    import help
+
+#main class
+class main():
+    #this is where all submodules need to speak to each other and we run loops
+    
