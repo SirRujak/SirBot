97a842943e6d1b9fff080091d51d6a7741275f2d
avoloc <avoloc@hotmail.com>
2014-09-10 17:22:25 -0500

    initial commit of shutdown.py
    shutdown.py script cleans up before cleanly exiting runtime

--- dev/TOPSECRET/SirBot/lib/sirbot/shutdown.py
+++ dev/TOPSECRET/SirBot/lib/sirbot/shutdown.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+
 #script containing all pertinent tasks to prepare for software termination.
 #successful completion of this process at last runtime, will skip extra validation
 #steps on next run
+
+def shutdown():
+    #check for lingering runtime errors
+    #if none: write clean.start file in config directory
+    pass
+
+#perhaps add garbage collector control here?
