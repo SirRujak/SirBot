92d0a09cfb232270d04f82eccc451ee63bd7901a
avoloc <avoloc@hotmail.com>
2015-02-28 16:34:15 -0600

    Revert "changes to config during runtime are now saved"
    This reverts commit e09a780da17bb2f97d20aafc2c007fe3fc3051bb.

--- dev/TOPSECRET/SirBot/lib/sirbot/shutdown.py
+++ dev/TOPSECRET/SirBot/lib/sirbot/shutdown.py
@@ -4,19 +4,10 @@
 #successful completion of this process at last runtime, will skip extra validation
 #steps on next run
 
-from json import dumps
-
-def shutdown(config,interinput=None,interoutput=None):
+def shutdown(config,interinput,interoutput):
     #check for lingering runtime errors
     #finishing writing log queues to file
     #if none: write clean.start file in config directory
-    if(config['Interface']['remember position'] == 0):
-        config['Interface']['map'] = '620x540+50+50'
-        
-    configPath = config['Path']+'\\config\\sirbot\\config'    
-    configFile = open(configPath,"wb+")
-    configData = dumps(config)
-    configFile.write(configData)
-    configFile.close()
+    pass
 
 #perhaps add garbage collector control here?
