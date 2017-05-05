25c1980c2f7471ed49edee0e04db85655d40ece3
RI <sirrujak@gmail.com>
2017-05-04 17:32:19 -0700

    Finishing up the setup section.
    Should have all the structs and logic for opening and loading everything needed in ai.go.

--- lib/sirbot/ai.go
+++ lib/sirbot/ai.go
@@ -1,6 +1,8 @@
 package sirbotlib
 
 import (
+	"encoding/json"
+	"errors"
 	"os"
 	"path/filepath"
 	"time"
@@ -11,11 +13,11 @@ type AI struct {
 	moduleName              string
 	boundChannel            string
 	botName                 string
-	commandDictFile         os.File
+	commandDictFile         *os.File
 	inputChannel            *chan Message
 	outputChannel           *chan Message
-	twitchDictFile          os.File
-	timerDictFile           os.File
+	twitchDictFile          *os.File
+	timerDictFile           *os.File
 	pathComName             string
 	pathDefaultComName      string
 	pathTimerName           string
@@ -33,14 +35,16 @@ type AI struct {
 	lastSave                time.Time
 	lastUserSave            time.Time
 	lastQuote               time.Time
-	commandDictionary       interface{} //TODO
-	timerDictionary         interface{} //TODO
-	spamDictionary          interface{} //TODO
-	userLevelDictionary     interface{} //TODO
-	twitchCommandDictionary interface{} //TODO
+	commandDictionary       *CommandDict
+	timerList               []*Timer
+	twitchCommandDictionary *TwitchDict
 	config                  *Config
 	channelName             string
 	paths                   pathHolder
+	// This was never completed.
+	spamDictionary interface{} //TODO
+	// I don't know if these are needed:
+	//userLevelDictionary     interface{}
 }
 
 type pathHolder struct {
@@ -62,12 +66,12 @@ type pathHolder struct {
 
 // Startup is the constructor for AI. It returns a pointer to the generated
 // AI system.
-func Startup(c *Config, ud *UserDict) (*AI, error) {
+func Startup(c *Config, ud *CommandDict) (*AI, error) {
 	var a AI
 	a.config = c
 	a.botName = c.twAccounts.automatedAccount.name
 	a.channelName = c.twChannels.defalutChannel
-	a.userDict = ud.users[a.channelName]
+	a.userDict = ud.commands[a.channelName]
 	a.paths.basePath = c.basePath
 	a.currentLine = 0
 	a.lastSave = time.Now()
@@ -88,12 +92,225 @@ func (a *AI) getCurrentTime() {
 }
 
 func (a *AI) loadData() error {
-	err := os.MkdirAll(a.paths.pathCommandFolders, 0655)
+	cmdDict, err := loadCommands(a.paths.pathCommandFolders,
+		a.paths.pathCommandName, a.paths.pathDefaultCommandName)
 	if err != nil {
 		return err
 	}
-
+	a.commandDictionary = cmdDict
+	quoteDict, err := loadQuotes(a.paths.pathQuotesFolders, a.paths.pathQuotes,
+		a.paths.pathDefaultQuotes)
+	if err != nil {
+		return err
+	}
+	a.quoteDict = quoteDict
+	userDict, err := loadUsers(a.paths.pathUsersFolders, a.paths.pathUserName,
+		a.paths.pathDefaultUsers)
+	if err != nil {
+		return err
+	}
+	a.userDict = userDict
+	timerList, err := loadTimers(a.paths.pathTimerFolders,
+		a.paths.pathTimerName, a.paths.pathDefaultTimerName)
+	if err != nil {
+		return err
+	}
+	a.timerList = timerList
 	return nil
+}
+
+func loadFile(s1, s2 string) (*os.File, error) {
+	err := os.MkdirAll(s1, 0660)
+	if err != nil {
+		return nil, err
+	}
+	tempFile, err := os.OpenFile(s2,
+		os.O_RDWR|os.O_CREATE, 0660)
+	if err != nil {
+		return nil, err
+	}
+	return tempFile, nil
+}
+
+func loadString(f *os.File) ([]byte, bool, error) {
+	var tempString []byte
+	tempLength, err := f.Read(tempString)
+	if err != nil {
+		return nil, false, err
+	}
+	if tempLength != 0 {
+		return tempString, true, nil
+	}
+	return tempString, false, nil
+}
+
+func loadTimers(s1, s2, s3 string) ([]*Timer, error) {
+	tempTimerFile, err := loadFile(s1, s2)
+	if err != nil {
+		return nil, err
+	}
+	defer tempTimerFile.Close()
+	var tempTimerList []*Timer
+	tempString, length, err := loadString(tempTimerFile)
+	if err != nil {
+		return nil, err
+	}
+	if length != true {
+		json.Unmarshal(tempString, &tempTimerList)
+	} else {
+		tempTimerFile, err := loadFile(s1, s3)
+		if err != nil {
+			return nil, err
+		}
+		defer tempTimerFile.Close()
+		tempString, length, err := loadString(tempTimerFile)
+		if err != nil {
+			return nil, err
+		}
+		if length {
+			json.Unmarshal(tempString, &tempTimerList)
+		} else {
+			return nil,
+				errors.New("Unable to load default quote dict in ai.go")
+		}
+	}
+	return tempTimerList, nil
+}
+
+func loadTwitchCommands(s1, s2, s3 string) (*TwitchDict, error) {
+	tempTwitchDictFile, err := loadFile(s1, s2)
+	if err != nil {
+		return nil, err
+	}
+	defer tempTwitchDictFile.Close()
+	var tempTwitchDict *TwitchDict
+	var tempString []byte
+	tempLength, err := tempTwitchDictFile.Read(tempString)
+	if err != nil {
+		return nil, err
+	}
+	if tempLength != 0 {
+		json.Unmarshal(tempString, &tempTwitchDict)
+	} else {
+		tempTwitchDictFile, err := loadFile(s1, s3)
+		if err != nil {
+			return nil, err
+		}
+		defer tempTwitchDictFile.Close()
+		tempString, length, err := loadString(tempTwitchDictFile)
+		if err != nil {
+			return nil, err
+		}
+		if length {
+			json.Unmarshal(tempString, &tempTwitchDict)
+		} else {
+			return nil,
+				errors.New("Unable to load default quote dict in ai.go")
+		}
+	}
+	return tempTwitchDict, nil
+}
+
+func loadQuotes(s1, s2, s3 string) (*QuoteDict, error) {
+	tempQuoteFile, err := loadFile(s1, s2)
+	if err != nil {
+		return nil, err
+	}
+	defer tempQuoteFile.Close()
+	var tempQuoteDict *QuoteDict
+	var tempString []byte
+	tempLength, err := tempQuoteFile.Read(tempString)
+	if err != nil {
+		return nil, err
+	}
+	if tempLength != 0 {
+		json.Unmarshal(tempString, &tempQuoteDict)
+	} else {
+		tempQuoteFile, err := loadFile(s1, s3)
+		if err != nil {
+			return nil, err
+		}
+		defer tempQuoteFile.Close()
+		tempString, length, err := loadString(tempQuoteFile)
+		if err != nil {
+			return nil, err
+		}
+		if length {
+			json.Unmarshal(tempString, &tempQuoteDict)
+		} else {
+			return nil,
+				errors.New("Unable to load default quote dict in ai.go")
+		}
+	}
+	return tempQuoteDict, nil
+}
+
+func loadUsers(s1, s2, s3 string) (*UserDictionary, error) {
+	tempUserFile, err := loadFile(s1, s2)
+	if err != nil {
+		return nil, err
+	}
+	defer tempUserFile.Close()
+	var tempUserDict *UserDictionary
+	tempString, length, err := loadString(tempUserFile)
+	if err != nil {
+		return nil, err
+	}
+	if length != true {
+		json.Unmarshal(tempString, &tempUserDict)
+	} else {
+		tempUserFile, err := loadFile(s1, s3)
+		if err != nil {
+			return nil, err
+		}
+		defer tempUserFile.Close()
+		tempString, length, err := loadString(tempUserFile)
+		if err != nil {
+			return nil, err
+		}
+		if length {
+			json.Unmarshal(tempString, &tempUserDict)
+		} else {
+			return nil,
+				errors.New("Unable to load default quote dict in ai.go")
+		}
+	}
+	return tempUserDict, nil
+}
+
+func loadCommands(s1, s2, s3 string) (*CommandDict, error) {
+	tempDictFile, err := loadFile(s1, s2)
+	if err != nil {
+		return nil, err
+	}
+	defer tempDictFile.Close()
+	var tempCommandDict *CommandDict
+	var tempString []byte
+	tempLength, err := tempDictFile.Read(tempString)
+	if err != nil {
+		return nil, err
+	}
+	if tempLength != 0 {
+		json.Unmarshal(tempString, &tempCommandDict)
+	} else {
+		tempDictFile, err := loadFile(s1, s3)
+		if err != nil {
+			return nil, err
+		}
+		defer tempDictFile.Close()
+		tempLength, err := tempDictFile.Read(tempString)
+		if err != nil {
+			return nil, err
+		}
+		if tempLength != 0 {
+			json.Unmarshal(tempString, &tempCommandDict)
+		} else {
+			return nil,
+				errors.New("Unable to load default command dict in ai.go")
+		}
+	}
+
+	return tempCommandDict, nil
 }
 
 func (p *pathHolder) makeDictPathName(channelName string) {
--- lib/sirbot/baseStructs.go
+++ lib/sirbot/baseStructs.go
@@ -1,9 +1,16 @@
 package sirbotlib
 
+import "time"
+
 // Message is the data struct for passing messages
 // between components.
 type Message struct {
-	data string
+	userName        string
+	channelName     string
+	timeStamp       *time.Time
+	messageContents string
+	isLocal         bool
+	chatType        int
 }
 
 // Config holds all of the base configuration data for
@@ -65,12 +72,86 @@ type account struct {
 	joinChat bool
 }
 
-// UserDict keeps a map of users and their command dictionaries.
-type UserDict struct {
-	users map[string]*DictNode
+// CommandDict keeps a map of users and their command dictionaries.
+type CommandDict struct {
+	commands       map[string]*CommandNode
+	levels         []string
+	outlinks       []string
+	linkDict       map[string]map[string][]string
+	responseDict   map[string]*responseLevel
+	conditions     map[string]string
+	links          map[string]*linkHolder
+	levelStructure map[interface{}]interface{} //TODO
 }
 
-// DictNode represents one node within the command dictionary structure.
-type DictNode struct {
-	nodes map[string]*DictNode
+// CommandNode represents one node within the command dictionary structure.
+type CommandNode struct {
+	nodes     map[string]*CommandNode
+	link      string
+	limits    map[string]*limitContainer
+	active    bool
+	isGlobal  bool
+	isCommand bool
+}
+
+type linkHolder struct {
+	singleLink map[string]*linkData
+}
+
+type linkData struct {
+	lastEditor string
+	creator    string
+	linkType   string
+	timeLimit  int
+	lineLimit  int
+	response   string
+	groups     map[string]bool
+	users      map[string]bool
+}
+
+type responseLevel struct {
+	responseIn  []string
+	responseOut []string
+}
+
+type limitContainer struct {
+	lastTime time.Time
+	lastLine int
+	total    int
+}
+
+// UserDictionary keeps data about all of the known users in chat.
+type UserDictionary struct {
+	users []user
+}
+
+type user struct {
+	groups []string
+	level  string
+}
+
+// QuoteDict holds a list of quotes and a map of the same quotes.
+type QuoteDict struct {
+	quoteList []*quote
+	quotes    map[string]bool
+}
+
+type quote struct {
+	quoteData    string
+	quoteCreator string
+}
+
+// TwitchDict contains a map of users commands to twitch specific commands.
+type TwitchDict struct {
+	twitchCommands map[string]string
+}
+
+// Timer holds the specifications for a single timer element.
+type Timer struct {
+	totalTime time.Duration
+	hours     time.Duration
+	minutes   time.Duration
+	seconds   time.Duration
+	active    bool
+	command   string
 }
